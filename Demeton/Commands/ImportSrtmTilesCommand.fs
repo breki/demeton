c8efecaa224e5ac09b5d1fd0a7f20c457cc780f6
Igor Brejc <igor.brejc@gmail.com>
2019-09-19 19:28:18 +0200

    Documented `Demeton.Commands.ImportSrtmTilesCommand.import` function.

--- Demeton/Commands/ImportSrtmTilesCommand.fs
+++ Demeton/Commands/ImportSrtmTilesCommand.fs
@@ -6,6 +6,24 @@ open System.IO
 
 type SrtmToPngEncoder = HeightsArray -> Stream -> unit
 
+/// <summary>
+/// A generic method for importing the specified SRTM tiles into the local
+/// storage, encoded as PNG files.
+/// </summary>
+/// <param name="tiles">A sequence of SRTM tiles to import.</param>
+/// <param name="readTile">
+/// A function that transforms a SRTM tile into <see cref="HeightsArray" />.
+/// This typically means the function should download the zipped HGT file,
+/// unzip it and read it as <see cref="HeightsArray" /> instance.
+/// </param>
+/// <param name="createPngTileFile">
+/// A function that creates a new PNG file for the given SRTM tile ID and
+/// provides a writable stream to it.
+/// </param>
+/// <param name="encodeSrtmTileToPng">
+/// A function that encodes the <see cref="HeightsArray" /> as a PNG image and
+/// writes it into the provides stream.
+/// </param>
 let import 
     (tiles: SrtmTileHgtFile seq)
     (readTile: SrtmTileReader)
