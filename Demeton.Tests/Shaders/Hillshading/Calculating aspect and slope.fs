f1ccc137814146a23f703e7227e6cd13b7aadb7c
Igor Brejc <igor.brejc@comtrade.com>
2019-10-08 22:06:17 +0200

    Redesigned the `slope` function.

--- Demeton.Tests/Shaders/Hillshading/Calculating aspect and slope.fs
+++ Demeton.Tests/Shaders/Hillshading/Calculating aspect and slope.fs
@@ -5,7 +5,7 @@ open Demeton.Shaders.Terrain
 open Xunit
 open FsCheck
 open PropertiesHelp
-
+open Swensen.Unquote
 
 [<Fact>]
 let ``Calculating slope``() =
@@ -20,17 +20,20 @@ let ``Calculating slope``() =
             (not heightsAreMissing && slopeValue >= 0. && slopeValue <= 90.)
 
     let ``is 90-degrees simetric`` (heightsWindow: HeightsWindow) =
+        let hDist = 100.
+        let vDist = 150.
+
         let rotate (heightsWindow: HeightsWindow)  =
             [| 
                 heightsWindow.[2]; heightsWindow.[0]; 
                 heightsWindow.[3]; heightsWindow.[1] |]
 
-        let originalSlopeValue = slope heightsWindow 100. 150.
-        let rotated90Value = slope (rotate heightsWindow) 100. 150.
+        let originalSlopeValue = slope heightsWindow hDist vDist
+        let rotated90Value = slope (rotate heightsWindow) vDist hDist 
         let rotated180Value = 
-            slope (rotate heightsWindow |> rotate) 100. 150.
+            slope (rotate heightsWindow |> rotate) hDist vDist
         let rotated270Value = 
-            slope (rotate heightsWindow |> rotate |> rotate) 100. 150.
+            slope (rotate heightsWindow |> rotate |> rotate) vDist hDist 
         originalSlopeValue = rotated90Value
             && originalSlopeValue = rotated180Value
             && originalSlopeValue = rotated270Value
@@ -46,3 +49,8 @@ let ``Calculating slope``() =
     genHeightsWindow |> Arb.fromGen 
     |> Prop.forAll <| specs
     |> Check.VerboseThrowOnFailure
+
+[<Fact>]
+let ``Some control value``() =
+    let heights = [| Some 0.; Some 0.; Some 100.; Some 100.; |]
+    test <@ slope heights 150. 100. = Some 45. @>
--- Demeton/Shaders/Terrain.fs
+++ Demeton/Shaders/Terrain.fs
@@ -13,23 +13,37 @@ let slope (heightsWindow: HeightsWindow) horizontalSize verticalSize =
     match someHeightsAreMissing heightsWindow with
     | true -> None
     | false ->
-        let diagonalSize = 
-            sqrt (Math.Pow(horizontalSize, 2.) + Math.Pow(verticalSize, 2.))
-    
-        let diagonalSlope height1 height2 =
+        let edgeSlope height1 height2 edgeSize =
             let minHeight = min height1 height2
             let maxHeight = max height1 height2
             let diffHeight = maxHeight - minHeight
-            let angleRad = Math.Atan2(diffHeight, diagonalSize)
+            let angleRad = Math.Atan2(diffHeight, edgeSize)
             radToDeg angleRad
 
-        let slope1 = 
-            diagonalSlope 
+        let slope01 = 
+            edgeSlope 
                 (Option.get (heightsWindow.[0])) 
+                (Option.get (heightsWindow.[1]))
+                horizontalSize
+        let slope23 = 
+            edgeSlope
+                (Option.get (heightsWindow.[2])) 
                 (Option.get (heightsWindow.[3]))
-        let slope2 = 
-            diagonalSlope 
-                (Option.get (heightsWindow.[1])) 
-                (Option.get (heightsWindow.[2]))
+                horizontalSize
 
-        Some ((slope1 + slope2) / 2.)
+        let horizontalAvgSlope = (slope01 + slope23) / 2.
+
+        let slope02 = 
+            edgeSlope 
+                (Option.get (heightsWindow.[0])) 
+                (Option.get (heightsWindow.[2]))
+                verticalSize
+        let slope13 = 
+            edgeSlope
+                (Option.get (heightsWindow.[1])) 
+                (Option.get (heightsWindow.[3]))
+                verticalSize
+
+        let verticalAvgSlope = (slope02 + slope13) / 2.
+
+        Some (max horizontalAvgSlope verticalAvgSlope)
