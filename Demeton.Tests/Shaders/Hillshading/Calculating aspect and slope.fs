8a723b51f4e9cb7054b9b2229158e160bef44c03
Igor Brejc <igor.brejc@gmail.com>
2019-10-10 07:19:08 +0200

    Implemented the reference slope function.

--- Demeton.Tests/Shaders/Hillshading/Calculating aspect and slope.fs
+++ Demeton.Tests/Shaders/Hillshading/Calculating aspect and slope.fs
@@ -5,12 +5,12 @@ open Demeton.Geometry.Common
 open Demeton.Geometry.D3
 
 open System
-open System.Diagnostics
 
 open Xunit
 open FsCheck
 open PropertiesHelp
 open Swensen.Unquote
+open TestHelp
 
 type SlopeCalculator = HeightsWindow -> float -> float -> float option
 
@@ -20,14 +20,12 @@ let vDist = 150.
 let referenceSlopeCalculator 
     (heightsWindow: HeightsWindow) horizontalSize verticalSize = 
     let triangleNormalToSlopeAndOrientation normal =
-        let slopeUnadjusted = Math.Atan2(normal.Y, normal.Z)
+        let normalXYLen = Math.Sqrt(normal.X * normal.X + normal.Y * normal.Y)
+
+        let slope = Math.Atan2(normalXYLen, normal.Z)
         let orientationUnadjusted = Math.Atan2(normal.X, normal.Z)
 
-        let slope = Math.Abs(slopeUnadjusted)
         let orientation = orientationUnadjusted
-
-        if normal.Z > 0. then Debugger.Break()
-        if slope <= Math.PI / 2. then Debugger.Break()
 
         (slope, orientation)
 
@@ -37,20 +35,28 @@ let referenceSlopeCalculator
         let points = [|
             { X = 0.; Y = 0.; Z = Option.get heightsWindow.[0] }
             { X = horizontalSize; Y = 0.; Z = Option.get heightsWindow.[1] }
-            { X = 0.; Y = verticalSize; Z = Option.get heightsWindow.[2] }
             { X = horizontalSize; Y = verticalSize; 
-                Z = Option.get heightsWindow.[3] }
+                Z = Option.get heightsWindow.[2] }
+            { X = 0.; Y = verticalSize; Z = Option.get heightsWindow.[3] }
         |]
 
         let triangle1Normal = triangleNormal points.[0] points.[1] points.[2]
-        let triangle2Normal = triangleNormal points.[1] points.[3] points.[2] 
+        let triangle2Normal = triangleNormal points.[2] points.[3] points.[0] 
+        let triangle3Normal = triangleNormal points.[0] points.[1] points.[3]
+        let triangle4Normal = triangleNormal points.[2] points.[3] points.[1]
+
         let (slope1, orientation1) = 
             triangleNormalToSlopeAndOrientation triangle1Normal
         let (slope2, orientation2) = 
             triangleNormalToSlopeAndOrientation triangle2Normal
+        let (slope3, orientation3) = 
+            triangleNormalToSlopeAndOrientation triangle3Normal
+        let (slope4, orientation4) = 
+            triangleNormalToSlopeAndOrientation triangle4Normal
 
-        let slope = (slope1 + slope2) / 2.
-        let orientation = (orientation1 + orientation2) / 2.
+        let slope = (slope1 + slope2 + slope3 + slope4) / 4.
+        let orientation = 
+            (orientation1 + orientation2 + orientation3 + orientation4) / 4.
 
         Some slope
 
@@ -79,28 +85,34 @@ let ``is 90-degrees symmetric``
     (heightsWindow: HeightsWindow)
     (slope: SlopeCalculator) =
 
+    let isApproxEqual a b =
+        (Option.isNone a && Option.isNone a = Option.isNone b) 
+            || (Option.get a =~= Option.get b)
+
     let rotate (heightsWindow: HeightsWindow)  =
-        [| 
-            heightsWindow.[2]; heightsWindow.[0]; 
-            heightsWindow.[3]; heightsWindow.[1] |]
+        [| heightsWindow.[3]; heightsWindow.[0]; 
+            heightsWindow.[1]; heightsWindow.[2] |]
 
     let originalValue = slope heightsWindow hDist vDist
-    let rotated90Value = slope (rotate heightsWindow) vDist hDist 
-    let rotated180Value = 
-        slope (rotate heightsWindow |> rotate) hDist vDist
-    let rotated270Value = 
-        slope (rotate heightsWindow |> rotate |> rotate) vDist hDist 
+
+    let heights90 = rotate heightsWindow
+    let heights180 = rotate heights90
+    let heights270 = rotate heights180
+
+    let rotated90Value = slope heights90 vDist hDist 
+    let rotated180Value = slope heights180 hDist vDist
+    let rotated270Value = slope heights270 vDist hDist 
     
-    let is90Sym = originalValue = rotated90Value
-    let is180Sym = originalValue = rotated180Value
-    let is270Sym = originalValue = rotated270Value
+    let is90Sym = isApproxEqual originalValue rotated90Value
+    let is180Sym = isApproxEqual originalValue rotated180Value
+    let is270Sym = isApproxEqual originalValue rotated270Value
     (is90Sym && is180Sym && is270Sym)
         |@ sprintf 
-            "is not symmetric: original:%f, 90:%f, 180:%f, 270:%f" 
-            (radToDeg (Option.get originalValue))
-            (radToDeg (Option.get rotated90Value))
-            (radToDeg (Option.get rotated180Value))
-            (radToDeg (Option.get rotated270Value))
+            "is not symmetric: original:%A, 90:%A, 180:%A, 270:%A" 
+            originalValue
+            rotated90Value
+            rotated180Value
+            rotated270Value
 
 let ``Calculates the same value as the reference implementation``
     (heightsWindow: HeightsWindow)
@@ -123,12 +135,17 @@ let ``Calculating slope using method 1``() =
     |> Prop.forAll <| specs slope
     |> Check.VerboseThrowOnFailure
 
-[<Fact>]
-let ``Some control value``() =
-    let heights = [| Some 0.; Some 0.; Some 100.; Some 100.; |]
-    test <@ slope heights 150. 100. = Some 45. @>
+[<Theory>]
+[<InlineData(0., 0., 100., 100., 150., 100., 45.)>]
+[<InlineData(100., 0., 0., 100., 100., 150., 45.)>]
+[<InlineData(100., 100., 0., 0., 150., 100., 45.)>]
+[<InlineData(0., 100., 100., 0., 100., 150., 45.)>]
+let ``Some control values`` h1 h2 h3 h4 hDist vDist expectedSlope =
+    let heights = [| Some h1; Some h2; Some h3; Some h4; |]
+    test <@ referenceSlopeCalculator heights hDist vDist 
+        = Some (degToRad expectedSlope) @>
 
-[<Fact(Skip="todo working on this one")>]
+[<Fact>]
 let ``Calculating slope using reference implementation``() =
     let genHeight = floatInRange -100 500
     let genHeightMaybe = genHeight |> optionOfWithFrequency 1
