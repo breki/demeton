9ba5e2898c57cf925310391d55519bd3a0969525
Igor Brejc <igor.brejc@gmail.com>
2019-10-26 10:26:23 +0200

    Moved shading pipeline code from the test module to the production one.

--- Demeton.Tests/Shaders/Shading pipeline.fs
+++ Demeton.Tests/Shaders/Shading pipeline.fs
@@ -1,44 +1,13 @@
 ﻿module Tests.Shaders.``Shading pipeline``
 
+open Demeton.Shaders.ShadingPipeline
 open Raster
 open Demeton.Shaders.Types
 open Png
 
 open Xunit
 open Swensen.Unquote
-open Png
 open Demeton.DemTypes
-
-
-type ShadingStep =
-    | Shading of RasterShader
-    | Compositing of 
-        (ShadingStep * ShadingStep * AlphaCompositing.CompositingFunc)
-
-
-let rec executeShadingStep 
-    heightsArray
-    tileRect
-    shaderOptions
-    (step: ShadingStep)
-    : RawImageData =
-
-    match step with
-    | Shading rasterShader -> 
-        let imageData =
-            Rgba8Bit.createImageData 
-                tileRect.Width tileRect.Height Rgba8Bit.ImageDataZero
-
-        rasterShader heightsArray tileRect imageData shaderOptions
-        imageData
-
-    | Compositing (step1, step2, compositingFunc) ->
-        let image1 = 
-            executeShadingStep heightsArray tileRect shaderOptions step1
-        let image2 = 
-            executeShadingStep heightsArray tileRect shaderOptions step2
-
-        compositingFunc tileRect.Width tileRect.Height image1 image2
 
 
 [<Fact>]
---
+++ Demeton/Shaders/ShadingPipeline.fs
@@ -0,0 +1,36 @@
+﻿module Demeton.Shaders.ShadingPipeline
+
+open Raster
+open Demeton.Shaders.Types
+open Png
+
+
+type ShadingStep =
+    | Shading of RasterShader
+    | Compositing of 
+        (ShadingStep * ShadingStep * AlphaCompositing.CompositingFunc)
+
+
+let rec executeShadingStep 
+    heightsArray
+    tileRect
+    shaderOptions
+    (step: ShadingStep)
+    : RawImageData =
+
+    match step with
+    | Shading rasterShader -> 
+        let imageData =
+            Rgba8Bit.createImageData 
+                tileRect.Width tileRect.Height Rgba8Bit.ImageDataZero
+
+        rasterShader heightsArray tileRect imageData shaderOptions
+        imageData
+
+    | Compositing (step1, step2, compositingFunc) ->
+        let image1 = 
+            executeShadingStep heightsArray tileRect shaderOptions step1
+        let image2 = 
+            executeShadingStep heightsArray tileRect shaderOptions step2
+
+        compositingFunc tileRect.Width tileRect.Height image1 image2
