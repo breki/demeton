682f0cc5c133e6c1e6e861c70ceadd2e781536be
Igor Brejc <igor.brejc@gmail.com>
2024-02-24 07:06:40 +0100

    Convert build.bat to build.ps1
    Update GH pipelines to use NET 8

---
+++ build.ps1
@@ -0,0 +1,40 @@
+$CONFIG = "Release"
+$BUILD_DIR = "builds\Demeton.Console"
+$SRTM_CACHE = "$PWD\samples\cache"
+
+Remove-Item -Recurse -Force "$BUILD_DIR"
+
+Write-Host "`nBUILDING THE CODE..."
+
+dotnet build --configuration $CONFIG --verbosity minimal --no-incremental
+if ($LASTEXITCODE -ne 0)
+{
+    exit $LASTEXITCODE
+}
+
+Write-Host "`nRUNNING THE TESTS..."
+
+dotnet test --configuration $CONFIG --verbosity minimal --filter "Category != acceptance"
+if ($LASTEXITCODE -ne 0)
+{
+    exit $LASTEXITCODE
+}
+
+# commented out the linting since fsharplint reports assembly loading error for some reason
+#Write-Host "RUNNING THE F# LINT..."
+#
+#dotnet dotnet-fsharplint.exe -sol Demeton.sln
+#if ($LASTEXITCODE -ne 0)
+#{
+#    exit $LASTEXITCODE
+#}
+
+Write-Host "`nMAKING THE CONSOLE PACKAGE..."
+
+dotnet publish Demeton.Console -c $CONFIG --output "$PWD\$BUILD_DIR"
+if ($LASTEXITCODE -ne 0)
+{
+    exit $LASTEXITCODE
+}
+
+Write-Host "`nBUILD SUCCESSFUL"
