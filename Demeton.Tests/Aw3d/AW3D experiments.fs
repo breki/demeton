016c0a3d5b923911c29cad447f21f77e3943a934
Igor Brejc <igor.brejc@gmail.com>
2024-02-27 22:36:11 +0100

    Experiment with WorldCover dataset

--- Demeton.Tests/Aw3d/AW3D experiments.fs
+++ Demeton.Tests/Aw3d/AW3D experiments.fs
@@ -2,7 +2,6 @@
 
 
 open System
-open System.IO
 open Demeton.Commands
 open Demeton.DemTypes
 open Demeton.Projections.PROJParsing
@@ -16,15 +15,15 @@ open BitMiracle.LibTiff.Classic
 open Swensen.Unquote
 
 
-let readAw3dHeightsFromStream (stream: Stream) : DemHeight[] =
-    use tiffFile = Tiff.ClientOpen("InMemory", "r", stream, TiffStream())
+let readAw3dHeights (fileName: string) : DemHeight[] =
+    use tiff = Tiff.Open(fileName, "r")
 
-    let width = unbox (tiffFile.GetField(TiffTag.IMAGEWIDTH).[0].Value)
+    let width = unbox (tiff.GetField(TiffTag.IMAGEWIDTH).[0].Value)
 
     if width <> 3600 then
         failwithf $"Expected width 3600, but got %d{width}"
 
-    let height = unbox (tiffFile.GetField(TiffTag.IMAGELENGTH).[0].Value)
+    let height = unbox (tiff.GetField(TiffTag.IMAGELENGTH).[0].Value)
 
     if height <> 3600 then
         failwithf $"Expected height 3600, but got %d{height}"
@@ -33,12 +32,12 @@ let readAw3dHeightsFromStream (stream: Stream) : DemHeight[] =
     let heightsArray: DemHeight[] = Array.zeroCreate arraySize
 
     let samplesPerPixel: int16 =
-        unbox (tiffFile.GetField(TiffTag.SAMPLESPERPIXEL).[0].Value)
+        unbox (tiff.GetField(TiffTag.SAMPLESPERPIXEL).[0].Value)
 
     let bitsPerSample: int16 =
-        unbox (tiffFile.GetField(TiffTag.BITSPERSAMPLE).[0].Value)
+        unbox (tiff.GetField(TiffTag.BITSPERSAMPLE).[0].Value)
 
-    let scanlineSize = tiffFile.ScanlineSize()
+    let scanlineSize = tiff.ScanlineSize()
     let buffer = Array.zeroCreate<byte> scanlineSize
 
     let pixelBytesSize = int samplesPerPixel * (int bitsPerSample / 8)
@@ -46,7 +45,7 @@ let readAw3dHeightsFromStream (stream: Stream) : DemHeight[] =
     let mutable heightsWrittenCount = 0
 
     for row in 0 .. height - 1 do
-        let success = tiffFile.ReadScanline(buffer, row)
+        let success = tiff.ReadScanline(buffer, row)
 
         if not success then
             failwithf $"Failed to read scanline %d{row}"
@@ -69,16 +68,16 @@ let readAw3dHeightsFromStream (stream: Stream) : DemHeight[] =
 
 [<Fact>]
 let ``Load AW3D into a DemHeight`` () =
-    use tiffReadStream = sampleFileStream @"AW3D.ALPSMLC30_N046E006_DSM.tif"
-    let demHeight = readAw3dHeightsFromStream tiffReadStream
+    let fileName = @"samples\ALPSMLC30_N046E007_DSM.tif"
+    let demHeight = readAw3dHeights fileName
 
     test <@ demHeight <> null @>
 
 let area, heights, srtmLevel, mapProjection, mapScale, tileRect =
     ShadingSampleGenerator.generateSampleWithParameters
-        6.1
+        7.1
         46.1
-        6.9
+        7.9
         46.9
         250000.
         72.
@@ -128,12 +127,13 @@ let customShadePixel
 
 [<Fact>]
 let ``Generate hillshading from AW3D sample file`` () =
-    use tiffReadStream = sampleFileStream @"AW3D.ALPSMLC30_N046E006_DSM.tif"
-    let demHeight = readAw3dHeightsFromStream tiffReadStream
+    let fileName = @"Samples\ALPSMLC30_N046E007_DSM.tif"
+
+    let demHeight = readAw3dHeights fileName
 
     let tileSize = 3600
 
-    let tileId = Demeton.Srtm.Funcs.parseTileName "N46E006"
+    let tileId = Demeton.Srtm.Funcs.parseTileName "N46E007"
     let cellMinX, cellMinY = Demeton.Srtm.Funcs.tileMinCell tileSize tileId
 
     let fetchHeightsArray tileIds =
@@ -150,8 +150,6 @@ let ``Generate hillshading from AW3D sample file`` () =
         |> Some
         |> Result.Ok
 
-    // todo 0: figure out how is the shader controlled - from the options
-    // or from the createShaderFunction?
     let pixelShader = customShadePixel IgorHillshader.defaultParameters
 
     let createShaderFunction _ =
@@ -166,7 +164,7 @@ let ``Generate hillshading from AW3D sample file`` () =
         ShadeCommand.saveShadedRasterTile
             FileSys.ensureDirectoryExists
             FileSys.openFileToWrite
-            Png.File.savePngToStream
+            File.savePngToStream
 
     let result = ShadeCommand.run options generateTile saveTile
     test <@ result |> isOk @>
---
+++ Demeton.Tests/WorldCover/WorldCover experiments.fs
@@ -0,0 +1,83 @@
+﻿module Tests.WorldCover.``WorldCover experiments``
+
+
+open System
+open Demeton.DemTypes
+open FsUnit
+open Xunit
+open BitMiracle.LibTiff.Classic
+open Swensen.Unquote
+
+// todo 5: this function should accept the SrtmTileId that needs to be read
+//   and then find the required TIFF tiles to fill the HeightsArray.
+//   We can then test the result by implementing a custom shader that renders
+//   just the water bodies. We will deal with mixing this together with AW3D
+//   later.
+
+
+let readWorldCoverRaster (fileName: string) : DemHeight[] =
+    use tiff = Tiff.Open(fileName, "r")
+
+    let width = unbox (tiff.GetField(TiffTag.IMAGEWIDTH).[0].Value)
+
+    if width <> 36000 then
+        failwithf $"Expected width 36000, but got %d{width}"
+
+    let height = unbox (tiff.GetField(TiffTag.IMAGELENGTH).[0].Value)
+
+    if height <> 36000 then
+        failwithf $"Expected height 36000, but got %d{height}"
+
+    let fieldValues = tiff.GetField(TiffTag.IMAGEDEPTH)
+    // looks like IMAGEDEPTH is not available in the file
+    // let imageDepth = unbox (fieldValues.[0].Value)
+
+    let planarConfig: PlanarConfig =
+        tiff.GetField(TiffTag.PLANARCONFIG).[0].Value :?> PlanarConfig
+
+    if planarConfig <> PlanarConfig.CONTIG then
+        failwithf
+            $"Expected CONTIG planar configuration, but got %A{planarConfig}"
+
+    if not (tiff.IsTiled()) then
+        failwithf "Expected a tiled TIFF file"
+
+    let tileWidth = unbox (tiff.GetField(TiffTag.TILEWIDTH).[0].Value)
+    let tileLength = unbox (tiff.GetField(TiffTag.TILELENGTH).[0].Value)
+
+    let buffer = Array.zeroCreate<byte> (tiff.TileSize())
+
+    for tileY in [ 0..tileLength..height ] do
+        for tileX in [ 0..tileWidth..width ] do
+            /// 0-based byte offset in buffer at which to begin storing read
+            /// and decoded bytes
+            let offset = 0
+            /// z-coordinate of the pixel within a tile to be read and decoded
+            let tileZ = 0
+            /// The zero-based index of the sample plane. The plane parameter is
+            /// used only if data are organized in separate planes
+            /// (PLANARCONFIG = SEPARATE). In other cases the value is ignored.
+            let plane = int16 0
+
+            // The tile to read and decode is selected by the (x, y, z, plane)
+            // coordinates (i.e. ReadTile returns the data for the tile
+            // containing the specified coordinates.
+            let bytesInTile =
+                tiff.ReadTile(buffer, offset, tileX, tileY, tileZ, plane)
+
+            if bytesInTile = -1 then
+                failwith "Could not read tile."
+
+    // Process the tile data stored in 'buffer'
+    // ...
+
+    raise (NotImplementedException())
+
+
+[<Fact(Skip = "in progress")>]
+let ``Load WorldCover file into a DemHeight`` () =
+    let raster =
+        readWorldCoverRaster
+            @"Samples\ESA_WorldCover_10m_2021_v200_N45E006_Map.tif"
+
+    test <@ raster <> null @>
