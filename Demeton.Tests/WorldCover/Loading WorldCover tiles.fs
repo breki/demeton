58b141c0ad8c458ce667612de4325c88ced7ea59
Igor Brejc <igor.brejc@gmail.com>
2024-04-29 22:27:59 +0200

    Download and cache WorldCover geoJSON file

--- Demeton.Tests/WorldCover/Loading WorldCover tiles.fs
+++ Demeton.Tests/WorldCover/Loading WorldCover tiles.fs
@@ -1,45 +1,132 @@
 ﻿module Tests.WorldCover.Loading_WorldCover_tiles
 
-open System.Dynamic
 open System.IO
 open FileSys
 
 open FsUnit
 open Newtonsoft.Json
+open Newtonsoft.Json.Linq
 open Xunit
 open Swensen.Unquote
+open TestHelp
 
 let year = 2021
 let version = "v200" // year 2021
 let s3UrlPrefix = "https://esa-worldcover.s3.eu-central-1.amazonaws.com"
+let geoJsonUrl = $"{s3UrlPrefix}/v100/2020/esa_worldcover_2020_grid.geojson"
+
+[<Literal>]
+let WorldCoverDirName = "WorldCover"
+
+/// <summary>
+/// Construct the file name for the cached WorldCover geoJSON file.
+/// </summary>
+let worldCoverGeoJsonCachedFileName cacheDir =
+    Path.Combine(
+        cacheDir,
+        WorldCoverDirName,
+        "esa_worldcover_2020_grid.geojson"
+    )
+
+
+let ensureGeoJsonFile
+    cacheDir
+    fileExists
+    (downloadFile: string -> string -> string)
+    =
+    let cachedFileName = worldCoverGeoJsonCachedFileName cacheDir
+
+    if fileExists cachedFileName then
+        cachedFileName
+    else
+        downloadFile geoJsonUrl cachedFileName
+
+
+
+/// <summary>
+/// List all available WorldCover tiles by reading the specified WorldCover
+/// geoJSON file.
+/// </summary>
+let listAllAvailableTiles (openFileToRead: FileWriter) (geoJsonFile: string) =
+    match openFileToRead geoJsonFile with
+    | Ok stream ->
+        use reader = new StreamReader(stream)
+        use jsonReader = new JsonTextReader(reader)
+        let serializer = JsonSerializer()
+        let jsonData = serializer.Deserialize<JObject>(jsonReader)
+
+        let features = jsonData["features"] :?> JArray
+
+        features
+        |> Seq.map (fun feature ->
+            let properties = (feature :?> JObject)["properties"] :?> JObject
+            properties["ll_tile"].Value<string>())
+
+    | Error e -> failwith e.Exception.Message
+
+[<Fact>]
+let ``Download WorldCover geoJSON file if it is not already cached`` () =
+    let cacheDir = "cache"
+
+    let geoJsonCachedFileName = worldCoverGeoJsonCachedFileName cacheDir
+
+    let fileExists =
+        function
+        | fileName when fileName = geoJsonCachedFileName -> false
+        | _ -> fail "Unexpected file name"
+
+    let mutable downloadFileCalls = 0
+
+    let downloadFile (url: string) (fileName: string) =
+        match url, fileName with
+        | url, fileName when
+            url = geoJsonUrl && fileName = geoJsonCachedFileName
+            ->
+            downloadFileCalls <- downloadFileCalls + 1
+            geoJsonCachedFileName
+        | _ -> fail "Unexpected URL or file name"
+
+
+    let resultingFileName = ensureGeoJsonFile "cache" fileExists downloadFile
+
+    test <@ resultingFileName = geoJsonCachedFileName @>
+    test <@ downloadFileCalls = 1 @>
 
 
 [<Fact>]
-let ``Icebreaker`` () =
+let ``Skip downloading WorldCover geoJSON file if it is already cached`` () =
+    let cacheDir = "cache"
+
+    let geoJsonCachedFileName = worldCoverGeoJsonCachedFileName cacheDir
+
+    let fileExists =
+        function
+        | fileName when fileName = geoJsonCachedFileName -> true
+        | _ -> fail "Unexpected file name"
+
+    let downloadFile (url: string) (fileName: string) =
+        fail "Should not be called"
+
+
+    let resultingFileName = ensureGeoJsonFile "cache" fileExists downloadFile
+
+    test <@ resultingFileName = geoJsonCachedFileName @>
+
+
+
+[<Fact>]
+let ``Can fetch the list of all available WorldCover tiles`` () =
     let geonJsonUrl =
         $"{s3UrlPrefix}/v100/2020/esa_worldcover_2020_grid.geojson"
 
     let geoJsonFile =
         downloadFile geonJsonUrl @"c:\temp\esa_worldcover_2020_grid.geojson"
 
-    // read geoJson file
-    let geoJsonObj =
-        match openFileToRead geoJsonFile with
-        | Ok stream ->
-            use reader = new StreamReader(stream)
-            use jsonReader = new JsonTextReader(reader)
-            let serializer = JsonSerializer()
-            serializer.Deserialize<ExpandoObject>(jsonReader)
-        | Error e -> failwith e.Exception.Message
+    let allAvailableTiles = listAllAvailableTiles openFileToRead geoJsonFile
 
-    // todo 0: fetch the set of all available WorldCover tiles
-    // features[0].properties.ll_tile S54E168
+    test <@ allAvailableTiles |> Seq.length = 2631 @>
 
-    // todo 4: the downloaded geojson file should be cached and retrieved from
-    //   the cache if it exists
 
-    // todo 8: we need to calculate the WorldCover tile for a given lat/lon
+// todo 8: we need to calculate the WorldCover tile for a given lat/lon
 
-    // todo 9: we need to calculate the list of tiles needed for given bounds
-
-    test <@ true @>
+// todo 9: we need to calculate the list of tiles needed for given bounds
